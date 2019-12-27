be33c57782e3c660fea46784e6e5e28b24ed94a4
Craig Storer <craigstorer@gmail.com>
2019-12-27 15:43:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,6 +35,8 @@ def adding
                  }
   shipping_manifest["muskets"] = 2    
   shipping_manifest["gun powder"] = 4
+  
+    return shipping_manifest
                  
                  
                  
