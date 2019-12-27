aff7644ecfd6eb7550d07024782c4958bdf94022
Craig Storer <craigstorer@gmail.com>
2019-12-27 15:42:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,7 +34,7 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2    
-  
+  shipping_manifest
                  
                  
                  
